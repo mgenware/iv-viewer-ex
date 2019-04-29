5fd8546e86246996b6bbf22579a5eb1afd980839
Mgen <m@mgenware.com>
2019-04-29 20:57:39 +0800

    Remove logging code

--- src/ImageViewer.ts
+++ src/ImageViewer.ts
@@ -208,10 +208,8 @@ export default class ImageViewer {
 
     let container = domElement;
     if (domElement.tagName.toLowerCase() === 'img') {
-      console.log(domElement);
       imageSrc =
         (domElement as HTMLImageElement).getAttribute('src') || 'NLLLL';
-      console.log(' src ', imageSrc);
       hiResImageSrc =
         domElement.getAttribute('high-res-src') ||
         domElement.getAttribute('data-high-res-src') ||
